4d1de07862645d9d7cd81c13fae891a0cb46c536
Francisco Javier Gutierrez Perez <frgutier@c3r5s2.42malaga.com>
2023-01-28 17:34:38 +0100

    tests leaks function
    Added the function for testing memory leaks in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 09:52:23 by frgutier          #+#    #+#             */
-/*   Updated: 2023/01/11 10:28:11 by frgutier         ###   ########.fr       */
+/*   Updated: 2023/01/28 17:28:30 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ void	check_arguments(int argc)
 	}
 }
 
+void	leaks(void)
+{
+	system("leaks -q fdf");
+}
+
 int	main(int argc, char **argv)
 {
 	t_fdf	*data;
@@ -32,5 +37,6 @@ int	main(int argc, char **argv)
 	draw(data);
 	mlx_key_hook(data->win_ptr, key_hook, data);
 	mlx_loop(data->mlx_ptr);
+	atexit(leaks);
 	return (0);
 }
