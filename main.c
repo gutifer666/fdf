518ccbb4a09eb28aded275e3bdc0f3b3c95690a4
Francisco Javier Gutierrez Perez <frgutier@c3r5s2.42malaga.com>
2023-01-31 08:13:28 +0100

    check arguments

--- main.c
+++ main.c
@@ -6,46 +6,27 @@
 /*   By: frgutier <frgutier@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 09:52:23 by frgutier          #+#    #+#             */
-/*   Updated: 2023/01/30 16:42:51 by frgutier         ###   ########.fr       */
+/*   Updated: 2023/01/31 08:11:09 by frgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"fdf.h"
 
-	/*
-void	check_arguments(int argc)
-{
-	if (argc != 2)
-	{
-		ft_printf("Utiliza una ruta de fichero válida: ./fdf <file_path>\n");
-		exit(1);
-	}
-}
-	*/
-/*
-	==23752==    by 0x1000035CF: allocate_z_matrix (read_file.c:54)
-	==23752==    by 0x100003755: read_map (read_file.c:97)
-	==23752==    by 0x100003414: main (main.c:32)
-*/
-/*
-valgrind --leak-check=full \
-         --show-leak-kinds=all \
-         --track-origins=yes \
-         --verbose \
-         --log-file=valgrind-out.txt \
-         ./fdf test_maps/42.fdf
-*/
-
 int	main(int argc, char **argv)
 {
 	t_fdf	*data;
-	(void)argc;
-	//check_arguments(argc);
-	data = (t_fdf *)malloc(sizeof(t_fdf));
-	read_map(argv[1], data);
-	init(data);
-	draw(data);
-	mlx_key_hook(data->win_ptr, key_hook, data);
-	mlx_loop(data->mlx_ptr);
+	int		check_fd;
+
+	if (argc == 2)
+	{
+		data = (t_fdf *)malloc(sizeof(t_fdf));
+		read_map(argv[1], data);
+		init(data);
+		draw(data);
+		mlx_key_hook(data->win_ptr, key_hook, data);
+		mlx_loop(data->mlx_ptr);
+	}
+	else
+		ft_printf("Error: Wrong number of arguments\n");
 	return (0);
 }
